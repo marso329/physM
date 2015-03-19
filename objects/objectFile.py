1188775abf014d674608e0f2fca93a64f0f8306a
marso329 <marso329@student.liu.se>
2015-03-19 21:29:05 +0100

    added missing files

---
+++ objects/objectFile.py
@@ -0,0 +1,60 @@
+from objects.objectSuperClass import *
+from constants.constants import PRIMITIVE_LOOKUP
+class objectFile(objectSuperClass):
+
+    def __init__(self,filename):
+        objectSuperClass.__init__(self)
+        self.vertices={}
+        self.number_of_vertices=1
+        self.primitive=None
+        self.primitive_set=False
+        self.primitive_number=0
+        self.faces={}
+        self.number_of_faces=0
+        for line in open(filename,"r"):
+            if line.startswith('#'): continue
+            values=line.split()
+            if not values: continue
+            if values[0]=="v":
+                v=list(map(float,values[1:4]))
+                v=v[0],v[1],v[2]
+                self.vertices[self.number_of_vertices]=v
+                self.number_of_vertices+=1
+            if values[0]=="f":
+                values=values[1:]
+                if not self.primitive_set:
+                    self.set_primitive(len(values))
+                assert(len(values)==self.primitive_number)
+                v=list(map(int,values[:self.primitive_number]))
+                temp=()
+                for i in range(self.primitive_number):
+                    temp=temp+(v[i],)
+                self.faces[self.number_of_faces]=temp
+                self.number_of_faces+=1
+        self.add_to_world()
+    def load(self):
+        self.update_everything()
+        glBegin(self.primitive)
+        if not self.transparency_enabled:
+            glColor3f(self.color[0],self.color[1],self.color[2])
+        else:
+            glColor4f(self.color[0],self.color[1],self.color[2],self.transparency)
+        for i in range(self.number_of_faces):
+            for j in range(self.primitive_number):
+                glVertex3fv(self.vertices[self.faces[i][j]])
+        glEnd()
+                
+                     
+    def set_primitive(self,x):
+        self.primitive=PRIMITIVE_LOOKUP[x]
+        self.primitive_set=True  
+        self.primitive_number=x  
+                
+                
+                
+                
+            
+            
+            
+            
+            
